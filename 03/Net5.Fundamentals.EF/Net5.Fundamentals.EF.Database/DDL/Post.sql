73361fb460f1e4362dc565c18e97af84b401bc29
Erick Arostegui Cunza <scorpius86@gmail.com>
2021-05-15 17:18:05 -0500

    Update DDL

---
+++ 03/Net5.Fundamentals.EF/Net5.Fundamentals.EF.Database/DDL/Post.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [Blog].[Post]
+(
+	PostId INT NOT NULL IDENTITY,
+	Titulo VARCHAR(200) NOT NULL,
+	Contenido VARCHAR(MAX) NOT NULL,
+	UsuarioIdPropietario INT NOT NULL,
+	UsuarioIdCreacion INT NOT NULL,
+	FechaCreacion DATETIME NOT NULL DEFAULT GETDATE(),
+	UsuarioIdActualizacion INT NOT NULL,
+	FechaActualizacion DATETIME NOT NULL DEFAULT GETDATE(),
+
+	CONSTRAINT PK_Post PRIMARY KEY (PostId),
+	CONSTRAINT FK_PostUsuarioPropietario FOREIGN KEY (UsuarioIdPropietario) REFERENCES Blog.Usuario(UsuarioId),
+	CONSTRAINT FK_PostUsuarioCreacion FOREIGN KEY (UsuarioIdCreacion) REFERENCES Blog.Usuario(UsuarioId),
+	CONSTRAINT FK_PostUsuarioActualizacion FOREIGN KEY (UsuarioIdActualizacion) REFERENCES Blog.Usuario(UsuarioId)
+
+)
